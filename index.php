2a067c7ff3b4828b34caf3c60180e3ef4ea86190
NodoMVP <nodarmk@gmail.com>
2019-07-19 21:38:38 +0400

    unfinished

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP CRUD</title>
+	<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="row justify-content-center">
+	<form action="" method="POST">
+		<div class="form-group">
+		<label>Name</label>
+		<input type="text" name="name" class="form-control" value="Enter your name">
+		</div>
+		<div class="form-group">
+		<label>Location</label>
+		<input type="text" name="location" class="form-control" value="enter your location">
+		</div>
+		<div class="form-group">
+		<button type="submit" class="btn btn-primary" name="save">Save</button>
+		</div>
+	</form>
+	</div>
+</body>
+</html>
